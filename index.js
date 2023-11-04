d738f1350de5bfd8b74791cfb4345751cd5b6ac9
Ajeet k. v. <ajeetipr@gmail.com>
2023-11-04 23:51:51 +0530

    ejs setup

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const express = require('express')
+const jwt =require("jsonwebtoken")
 const app = express()
-app.get('/', function (req, res) {
-  res.send('Hello World')
+let count = 0;
+
+
+
+
+//start link ejs npm
+app.set("view engine", "ejs");
+// add public folder
+app.use(express.static("./public"));
+// render html page
+app.get('/', function (req, res, next) {
+  res.render("index",{name:"Ajeet Kumar"});
 })
+
+//through error
+app.get('/error', function (req, res, next) {
+  throw Error("I useg the error url")
+  //after throw error it will find erre handler
+})
+//erre handler
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+})
+
 app.listen(3000)
